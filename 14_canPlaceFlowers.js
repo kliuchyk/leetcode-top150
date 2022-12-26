85f2df35b49173dba458f3e70fb095767dfb6b0b
ievgen.kliuchyk <kliuchyk@gmail.com>
2022-12-26 18:43:10 +0100

    add canPlaceFlowers

---
+++ 14_canPlaceFlowers.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// 0 - means empty
+// 1 - means not empty,
+
+function canPlaceFlowers(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (n === 0) {
+      return true;
+    }
+
+    if (flowerbed.length === 1 && flowerbed[0] === 0) {
+      return true;
+    }
+
+    // first item in row
+    if (i === 0) {
+      if (flowerbed[i] === 0 && flowerbed[i + 1] === 0) {
+        flowerbed.splice(i, 1, 1);
+        n--;
+      }
+    }
+
+    // last item in row
+    if (i === flowerbed.length - 1) {
+      if (flowerbed[i] === 0 && flowerbed[i - 1] === 0) {
+        flowerbed.splice(i, 1, 1);
+        n--;
+      }
+    }
+
+    if (flowerbed[i] === 0 && flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
+      flowerbed.splice(i, 1, 1);
+      n--;
+    }
+  }
+
+  return n === 0;
+}
+
+const flowerbed = [1, 0, 0, 0, 1],
+  n = 1;
+console.log(canPlaceFlowers(flowerbed, n));
