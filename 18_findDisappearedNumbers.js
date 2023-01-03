1f8d037fce74c49fa86400612914beb266443521
ievgen.kliuchyk <kliuchyk@gmail.com>
2023-01-03 18:25:51 +0100

    add solution for findDisappearedNumbers

---
+++ 18_findDisappearedNumbers.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var findDisappearedNumbers = function (nums) {
+  const arrWithNums = Array.from({ length: nums.length }, (_, i) => i + 1);
+  const uniqueValues = new Set(arrWithNums);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (uniqueValues.has(nums[i])) {
+      uniqueValues.delete(nums[i]);
+    }
+  }
+
+  return [...uniqueValues];
+};
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
