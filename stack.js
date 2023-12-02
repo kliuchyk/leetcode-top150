ace2080790adda25930e27ed34aa69c153023cec
ievgen.kliuchyk <kliuchyk@gmail.com>
2023-12-02 16:27:03 +0100

    add stack solution

---
+++ stack.js
@@ -0,0 +1,57 @@
+class Stack {
+  constructor() {
+    this.memory = [];
+    this.length = 0;
+  }
+
+  /**
+   * Pushes an item onto the top of the stack.
+   * @param {*} item The item to be pushed onto the stack.
+   * @return {number} The new length of the stack.
+   */
+  push(item) {
+    this.memory.push(item);
+    this.length += 1;
+    return this.length;
+  }
+
+  /**
+   * Remove an item at the top of the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  pop() {
+    return this.memory.pop();
+  }
+
+  /**
+   * Determines if the stack is empty.
+   * @return {boolean} `true` if the stack has no items, `false` otherwise.
+   */
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  /**
+   * Returns the item at the top of the stack without removing it from the stack.
+   * @return {*} The item at the top of the stack if it is not empty, `undefined` otherwise.
+   */
+  peek() {
+    return this.memory[this.length - 1];
+  }
+
+  /**
+   * Returns the number of items in the stack.
+   * @return {number} The number of items in the stack.
+   */
+  length() {
+    return this.memory.length;
+  }
+}
+
+const s = new Stack();
+
+s.push(100);
+s.push(200);
+console.log('stack: ', s);
+
+console.log('pop: ', s.pop());
